01c1410e2b3db923df2e86863f4f90f8fbe47a12
Sameer898 <>
2023-10-12 01:39:30 +0530

    Updated

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import {toast} from 'react-hot-toast'
+
+
+function App() {
+  return (
+    <div>
+     
+   home comming
+   
+    </div>
+    
+  );
+  }
+    
+   
+  
+
+
+export default App;
---
+++ src/reducer/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from '@reduxjs/toolkit'
+import React from 'react'
+
+export const rootReducer =combineReducers({
+   
+})
