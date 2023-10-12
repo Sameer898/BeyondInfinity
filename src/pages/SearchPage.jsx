76a6d3a0237d3d27d49b2d2333f9295ee3bf2abc
Sameer898 <sameerturke4@gmail.com>
2023-10-13 05:21:50 +0530

    updated

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Navbar } from './Navbar'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
+import {toast} from 'react-hot-toast'
 export const SearchPage = () => {
     const [firstSearch,setFirstSearch]=useState(true);
     const [secondSearch,setSecondSearch]=useState(false);
     const [confirmation,setConfirmation]=useState(false);
+    const navigate=useNavigate();
 
     const handleFirstSearch=()=>{
         setFirstSearch(true);
@@ -93,19 +95,45 @@ export const SearchPage = () => {
     <div className="ml-[30px] w-[165px] h-[58px] top-[1127px] left-[377px] font-inter font-[700px] text-[48px] leading-[58px] text-white">Caregivers</div>
     <div className="ml-[30px] w-[404px] mt-[20px] top-[169px] left-[352px] border-[1px] text-white "></div>
 
-    <div className=''>
-            <div className="flex ml-[30px] w-[1002px] h-[112px] top-[1261px] left-[270px] border-[1px] bg-[#010B13] text-white">
+    <div className='mt-[20px]'>
+            <div className="flex gap-[60px] ml-[30px] w-[1002px] h-[112px] top-[1261px] left-[270px] border-[1px] bg-[#010B13] text-white">
             <div className=''>
-                    <div className="w-[67px] h-[29px] top-[1281px] left-[311px] font-inter font-[400px] text-[24px] leading-[29px] text-white">Name</div>
-                    <div className='w-[164px] h-[29px] top-[317px] left-[311px] font-inter font-[400px] text-[24px] leading-[29px] text-[#808080]'>Harshal Tonge</div>
+                    <div className="ml-[10px] w-[67px] h-[29px] top-[1281px] left-[311px] font-inter font-[400px] text-[24px] leading-[29px] text-white">Name</div>
+                    <div className='ml-[10px] w-[164px] h-[29px] top-[317px] left-[311px] font-inter font-[400px] text-[24px] leading-[29px] text-[#808080]'>Harshal Tonge</div>
 
             </div>
             <div>
-                    <div className='w-[94px] '></div>
+                    <div className='ml-[10px] w-[94px] h-[29px] top-[1281px] left-[543px] font-inter font-[400px] text-[24px] leading-[29px] text-white'>Address</div>
+                    <div className='ml-[10px] w-[355px] h-[29px] top-[1317px] left-[543px] font-inter font-[400px] text-[24px] leading-[29px] text-[#808080]' >Hanuman nagar,Hingna,Nagpur</div>
+            </div>
+            <div className='flex gap-[30px] mt-[10px] '>
+            <button onClick={()=>handleConfirmation()} className="w-[116px] h-[50px] top-[1296px] text-black left-[946px] bg-[#50C878]">Request</button>
+            <button onClick={()=>navigate('/search')} className="w-[116px] h-[50px] top-[1296px] text-black left-[1108px] bg-[#ACE1AF]">Back</button>
             </div>
                     
             </div>  
+
+            <div className="flex gap-[60px] ml-[30px] w-[1002px] h-[112px] top-[1373px] left-[270px] border-[1px] bg-[#010B13] text-white">
+            <div className=''>
+                    <div className="ml-[10px] w-[67px] h-[29px] top-[1389px] left-[325px] font-inter font-[400px] text-[24px] leading-[29px] text-white">Name</div>
+                    <div className='ml-[10px] w-[164px] h-[29px] top-[1424px] left-[310px] font-inter font-[400px] text-[24px] leading-[29px] text-[#808080]'>Himanshu vade</div>
+
+            </div>
+            <div>
+                    <div className='ml-[10px] w-[94px] h-[29px] top-[1389px] left-[563px] font-inter font-[400px] text-[24px] leading-[29px] text-white'>Address</div>
+                    <div className='ml-[10px] w-[355px] h-[29px] top-[1424px] left-[544px] font-inter font-[400px] text-[24px] leading-[29px] text-[#808080]' >Hanuman nagar,Hingna,Nagpur</div>
+            </div>
+            <div className='flex gap-[30px] mt-[10px] '>
+            <button onClick={()=>handleConfirmation()} className="w-[116px] h-[50px] top-[1403px] text-black left-[946px] bg-[#50C878]">Request</button>
+            <button onClick={()=>navigate('/search')} className="w-[116px] h-[50px] top-[1403px] text-black left-[1108px] bg-[#ACE1AF]">Back</button>
+            </div>
+            
+                    
+            </div>  
+            <button onClick={()=>navigate('/search')} className="ml-[30px] mt-[50px] w-[116px] h-[50px] top-[1548px] text-black left-[270px] bg-[#ACE1AF]">Back</button>
+            
     </div>
+    
 
     </div>
 
@@ -113,7 +141,37 @@ export const SearchPage = () => {
 
        
     }
+    {
+        confirmation && 
+        <div className='flex'>
+    <div className="w-[272px] h-[862px] bg-[#010B13] text-center py-[76px] px-[-10px] border-[1px] text-white ">
+        <div className=' "'><Link to="/myprofile">Profile</Link></div>
+        <div className="w-[194px] text-center top-[94px] ml-[45px] ">--------------------</div>
+        <div className="w-[194px] text-center top-[94px] ml-[45px] "></div>
+        <div className='"'><Link to="/search">Search</Link></div>
+        <div className="w-[194px] text-center top-[94px] ml-[45px] ">--------------------</div>
+        <div className=' '><Link to="/authentication">Authentication</Link></div>
+        <div className="w-[194px] text-center mt-[200px] ml-[45px] text-center ">--------------------</div>
+       <Link to="/setting" className="">Setting</Link>
+    </div>
+        <div className="w-[639px] h-[348px] mx-auto border-[1px] bg-[#010B13] text-white">
+                <div className='text-[40px] rounded-3xl '>Are you sure you want to send Request</div>
+                <div className="flex">
+                <button onClick={()=>{
+                    handleConfirmation();
+                    toast.success("Successfully request sent.");
+                }} className="ml-[30px] mt-[50px] w-[116px] h-[50px] top-[1548px] text-black left-[270px] bg-[#ACE1AF]">Request</button>
+                <button onClick={()=>{
+                    handleFirstSearch();
+                    navigate('/search');
+                }} className="ml-[30px] mt-[50px] w-[116px] h-[50px] top-[1548px] text-black left-[270px] bg-[#ACE1AF]">Cancel</button>
+                </div>
+                
+        </div>
+        </div>
+    }
     
     </div>
+    
   )
 }
