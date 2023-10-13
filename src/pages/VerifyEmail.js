b7ae211e6c837c60025c9fcac80d87dafcaa6235
Sameer898 <sameerturke4@gmail.com>
2023-10-13 08:20:01 +0530

    update

--- src/pages/VerifyEmail.js
+++ src/pages/VerifyEmail.js
@@ -5,11 +5,12 @@ import { useSelector } from 'react-redux';
 export const VerifyEmail = () => {
     const navigate=useNavigate();
   return (
-    <div>
-    <img src="./assets/istockphoto-1191918617-612x612-removebg-preview.png" alt="logo" id="logo"/>
+    <div className="flex mx-auto px-[330px] ">
+    <img src="img/logo.png" alt="logo" id="logo"/>
     <div class="container">
         <div class="aside">
         <div class="input">
+        <div className="mt-[200px] ">
         <div class="header"><h4>VERIFY OTP</h4></div>
             <label for="OTP">OTP</label>
             <input type="text" id="OTP"/><br/>
@@ -17,6 +18,7 @@ export const VerifyEmail = () => {
                 
                 navigate('/login');
             }} id="btn" action="#">SUBMIT</button>
+        </div>
         </div>
         </div>
     </div>
