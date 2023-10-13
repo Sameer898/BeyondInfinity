6de74b231ca8dae169c546cc7ca4fdd412c63556
Sameer898 <sameerturke4@gmail.com>
2023-10-13 08:30:04 +0530

    updated

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux';
 export const Auth = () => {
     const navigate=useNavigate();
-    const {type}=useSelector();
+    const {type}=useSelector((state)=>(state).auth);
   return (
     <div>
         <Navbar/>
--- src/pages/VerifyEmail.js
+++ src/pages/VerifyEmail.js
@@ -5,16 +5,16 @@ import { useSelector } from 'react-redux';
 export const VerifyEmail = () => {
     const navigate=useNavigate();
   return (
-    <div className="flex mx-auto px-[330px] ">
-    <img src="img/logo.png" alt="logo" id="logo"/>
+    <div className="flex mx-auto px-[330px] bg-sky-300 h-[700px] ">
+    <img src="img/logo.png" className="w-[500px] " alt="logo" id="logo"/>
     <div class="container">
         <div class="aside">
         <div class="input">
-        <div className="mt-[200px] ">
-        <div class="header"><h4>VERIFY OTP</h4></div>
+        <div className="mt-[200px] flex flex-col gap-[20px] ">
+        <div class="header font-extrabold text-[30px] "><h4>VERIFY OTP</h4></div>
             <label for="OTP">OTP</label>
-            <input type="text" id="OTP"/><br/>
-            <button onClick={()=>{
+            <input type="text" className=" bg-zinc-500" placeholder="  Enter your otp" id="OTP"/><br/>
+            <button className="bg-green-600 w-[100px] px-[15px] rounded-lg" onClick={()=>{
                 
                 navigate('/login');
             }} id="btn" action="#">SUBMIT</button>
